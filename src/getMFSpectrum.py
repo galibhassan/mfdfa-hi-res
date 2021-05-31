c0fdf20e47ea79a9ab3be65fb1182b9c13f0f618
Galib Hassan <galib.hassan.0@gmail.com>
2021-05-31 05:15:24 +0200

    Process multiple data files

--- src/getMFSpectrum.py
+++ src/getMFSpectrum.py
@@ -5,7 +5,7 @@ from MFDFA import MFDFA
 import os
 
 DATA_DIR_MAIN = './sample_data_folder'
-DATA_PATH = f'{DATA_DIR_MAIN}/sample1.csv'
+
 PLOT_DIR_PATH = './images'
 OUTPUT_DIR = './output'
 
@@ -32,69 +32,73 @@ def makeOutputDir(dataFileName, output_dir):
     return newDirPath
     # print(newDirPath)
 
+dataFileNames =  os.listdir(DATA_DIR_MAIN)
+for dataFileName in dataFileNames:
+    pass
 
+    DATA_PATH = f'{DATA_DIR_MAIN}/{dataFileName}'
+    df = pd.read_csv(DATA_PATH, delimiter=";")
 
-dataFileName = 'sample.csv'
-df = pd.read_csv(DATA_PATH, delimiter=";")
+    tColumnName = df.columns[0]
+    dataSpanString = f'{df[tColumnName].iloc[0]} to {df[tColumnName].iloc[-1]}'
+    fColumnName = df.columns[1]
+    refFreq = int(fColumnName.split("_")[0].split("f")[1])
+    print(refFreq)
+    f_data = df[fColumnName].to_numpy()
+    t_domain = np.arange(0, len(f_data))*100/1000/60/60 # in hr
+    timeUnit = 'hour'
 
-tColumnName = df.columns[0]
-dataSpanString = f'{df[tColumnName].iloc[0]} - {df[tColumnName].iloc[-1]}'
-fColumnName = df.columns[1]
-refFreq = int(fColumnName.split("_")[0].split("f")[1])
-print(refFreq)
-f_data = df[fColumnName].to_numpy()
-t_domain = np.arange(0, len(f_data))*100/1000/60/60 # in hr
-timeUnit = 'hour'
+    # Create the output subfolder
+    outputSubDir =  makeOutputDir(dataFileName, OUTPUT_DIR)
 
-# Create the output subfolder
-outputSubDir =  makeOutputDir(dataFileName, OUTPUT_DIR)
-
-# plot of time vs frequency
-plt.plot(t_domain, f_data, label=f'Deviation from {refFreq}Hz' )
-plt.hlines(0, t_domain[0], t_domain[-1], colors='orange', linestyles='--', label=f'{refFreq}hz', data=None,)
-plt.xlabel(f"Time ({timeUnit})")
-plt.ylabel(f"Frequency Deviation (in mHz) from {refFreq}Hz")
-plt.title(f'''Time vs Frequency-deviation
-Time span: {dataSpanString}''')
-plt.legend()
-
-# saving figure
-tvsf_imageFileName = f't_vs_df__{dataFileName.split(".")[0]}'
-tvsf_imagePath = f'{outputSubDir}/{tvsf_imageFileName}'
-print(tvsf_imagePath)
-saveImage(tvsf_imagePath)
-
-# plt.show()
-
-
-
-
-
-
-# lag = np.unique(np.logspace(0.5, 3, 100).astype(int))
-# q_list = np.linspace(-10,10,41)
-# q_list = q_list[q_list!=0.0]
-# lag, dfa, dfa_std = MFDFA(X, lag, q = q, order = 1, stat = True, extensions = {"eDFA": True})
-""" 
-lag = np.unique(np.logspace(0.5, 30, 100).astype(int))
-# orders = np.linspace(0,10,5).astype(int)
-q_list = np.linspace(0,10,5)
-q_list = q_list[q_list != 0.0]
-orders = [0,1,2,3,4,5]
-for order in orders:
-    for q in q_list:
-        lag, dfa = MFDFA(f_data, lag=lag, q=q, order=order)
-        plt.loglog(lag, dfa, "-", label=f"q={q:.2f}, ord={order}")
-
+    # plot of time vs frequency
+    plt.plot(t_domain, f_data, label=f'Deviation from {refFreq}Hz' )
+    plt.ylim(-2000, 2000)
+    plt.hlines(0, t_domain[0], t_domain[-1], colors='orange', linestyles='--', label=f'{refFreq}hz', data=None,)
+    plt.xlabel(f"Time ({timeUnit})")
+    plt.ylabel(f"Frequency Deviation (in mHz) from {refFreq}Hz")
+    plt.title(f'''Time vs Frequency-deviation
+    Time span: {dataSpanString}
+    Data length: {len(f_data)} ''', loc='left')
     plt.legend()
-    plt.xlabel("lag s")
-    plt.ylabel(f"$F_q(s)$")
-    plt.title(f'''$F_q(s)$__order_{order}
-    data length = {len(f_data)}''')
+
+    # saving figure
+    tvsf_imageFileName = f't_vs_df__{dataFileName.split(".")[0]}'
+    tvsf_imagePath = f'{outputSubDir}/{tvsf_imageFileName}'
+    print(tvsf_imagePath)
+    saveImage(tvsf_imagePath)
+
     # plt.show()
 
-    filename = f'F_q(s)__order_{order}__q_many'
-    imageFileName=f'{PLOT_DIR_PATH}/{filename}.png'
 
-    saveImage(imageFileName)
- """
+
+
+
+
+    # lag = np.unique(np.logspace(0.5, 3, 100).astype(int))
+    # q_list = np.linspace(-10,10,41)
+    # q_list = q_list[q_list!=0.0]
+    # lag, dfa, dfa_std = MFDFA(X, lag, q = q, order = 1, stat = True, extensions = {"eDFA": True})
+    """ 
+    lag = np.unique(np.logspace(0.5, 30, 100).astype(int))
+    # orders = np.linspace(0,10,5).astype(int)
+    q_list = np.linspace(0,10,5)
+    q_list = q_list[q_list != 0.0]
+    orders = [0,1,2,3,4,5]
+    for order in orders:
+        for q in q_list:
+            lag, dfa = MFDFA(f_data, lag=lag, q=q, order=order)
+            plt.loglog(lag, dfa, "-", label=f"q={q:.2f}, ord={order}")
+
+        plt.legend()
+        plt.xlabel("lag s")
+        plt.ylabel(f"$F_q(s)$")
+        plt.title(f'''$F_q(s)$__order_{order}
+        data length = {len(f_data)}''')
+        # plt.show()
+
+        filename = f'F_q(s)__order_{order}__q_many'
+        imageFileName=f'{PLOT_DIR_PATH}/{filename}.png'
+
+        saveImage(imageFileName)
+    """
