3403e3d97796a137c1d80883fe3a7cd2cb01114e
Galib Hassan <galib.hassan.0@gmail.com>
2021-05-31 05:51:18 +0200

    Minor Refactor

--- src/getMFSpectrum.py
+++ src/getMFSpectrum.py
@@ -5,8 +5,6 @@ from MFDFA import MFDFA
 import os
 
 DATA_DIR_MAIN = './sample_data_folder'
-
-PLOT_DIR_PATH = './images'
 OUTPUT_DIR = './output'
 
 def saveImage(imagePath):
@@ -32,18 +30,16 @@ def makeOutputDir(dataFileName, output_dir):
     return newDirPath
     # print(newDirPath)
 
+# Files in the main data directory
 dataFileNames =  os.listdir(DATA_DIR_MAIN)
 for dataFileName in dataFileNames:
-    pass
-
-    DATA_PATH = f'{DATA_DIR_MAIN}/{dataFileName}'
-    df = pd.read_csv(DATA_PATH, delimiter=";")
+    dataPath = f'{DATA_DIR_MAIN}/{dataFileName}'
+    df = pd.read_csv(dataPath, delimiter=";")
 
     tColumnName = df.columns[0]
     dataSpanString = f'{df[tColumnName].iloc[0]} to {df[tColumnName].iloc[-1]}'
     fColumnName = df.columns[1]
     refFreq = int(fColumnName.split("_")[0].split("f")[1])
-    print(refFreq)
     f_data = df[fColumnName].to_numpy()
     t_domain = np.arange(0, len(f_data))*100/1000/60/60 # in hr
     timeUnit = 'hour'
@@ -79,12 +75,13 @@ for dataFileName in dataFileNames:
     # q_list = np.linspace(-10,10,41)
     # q_list = q_list[q_list!=0.0]
     # lag, dfa, dfa_std = MFDFA(X, lag, q = q, order = 1, stat = True, extensions = {"eDFA": True})
-    """ 
-    lag = np.unique(np.logspace(0.5, 30, 100).astype(int))
+    
+    lag = np.unique(np.logspace(0.5, 30, 20).astype(int))
     # orders = np.linspace(0,10,5).astype(int)
-    q_list = np.linspace(0,10,5)
+    # q_list = np.linspace(0,10,5)
+    q_list = np.array([1,2])
     q_list = q_list[q_list != 0.0]
-    orders = [0,1,2,3,4,5]
+    orders = [1]
     for order in orders:
         for q in q_list:
             lag, dfa = MFDFA(f_data, lag=lag, q=q, order=order)
@@ -95,10 +92,9 @@ for dataFileName in dataFileNames:
         plt.ylabel(f"$F_q(s)$")
         plt.title(f'''$F_q(s)$__order_{order}
         data length = {len(f_data)}''')
-        # plt.show()
 
         filename = f'F_q(s)__order_{order}__q_many'
+        PLOT_DIR_PATH = outputSubDir
         imageFileName=f'{PLOT_DIR_PATH}/{filename}.png'
 
         saveImage(imageFileName)
-    """
